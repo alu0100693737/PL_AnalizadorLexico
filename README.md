abf84577197d67985cc8e34036d94b4aa3cea80d
Ivan Garcia <alu0100693737@ull.edu.es>
2015-03-26 00:35:03 +0000

    Github Pages

---
+++ README.md
@@ -0,0 +1,28 @@
+# Practica 4. Analizador Lexico. #
+CSV ANALYZER    [![Build Status](https://travis-ci.org/alu0100693737/PL_AnalizadorLexico.svg)](https://travis-ci.org/alu0100693737/PL_AnalizadorLexico)
+
+Nota: esta pagina ha sido desarrollada usando Marckdown.
+--------------------------------------------------------
+
+### ***analizador lexico***
+
+http://alu0100693737.github.io/PL_csv/
+
+##Despliegue Heroku
+
+##Github pages and tests
+
+### Javascript application: Analizador Lexico
+
+### Participantes.
+###### Iván García Campos.
+
+Para probar gulp y karma
+npm install.
+
+Gulp: gulp test
+Karma: node_modules/karma/bin/karma start
+
+### JQuery 
+ jQuery is a cross-platform JavaScript library designed to simplify the client-side scripting of HTML. 
+###### Markdown es un lenguaje de marcado ligero parecido al que se emplea en muchas wikis y basado originalmente en convenciones existentes en el carcado de los correos electronicos. Emplea texto plano, procurando que sea legible pero consiguientdo que se convierta en XHTML correctamente formateado.
---
+++ tests/tests.js
@@ -0,0 +1,47 @@
+var assert = chai.assert;
+
+suite('Analizador lexico', function() {
+setup(function(){
+  if (typeof __html__ !== 'undefined') {
+  document.body.innerHTML = __html__['tests/index.html'];
+  input = document.getElementById('INPUT');
+  ouput = document.getElementById('OUTPUT');
+  }
+  });
+
+  test('Prueba declaracion de variable.', function() {
+  INPUT.value = 'var a = "hello";';
+  main();
+  assert.deepEqual(OUTPUT.innerHTML, '{\n    "value": "=",\n    "arity": "binary",\n    "first": {\n        "value": "a",\n        "arity": "name"\n    },\n    "second": {\n        "value": "hello",\n        "arity": "literal"\n    }\n}');
+  });
+  
+  test('Prueba declaracion de una funcion.', function() {
+  INPUT.value = 'var a = "hello"; // initialize a \nvar b = function(x) {var c = 3;return x+c; };';
+  main();
+  assert.deepEqual(OUTPUT.innerHTML, '[\n    {\n        "value": "=",\n        "arity": "binary",\n        "first": {\n            "value": "a",\n            "arity": "name"\n        },\n        "second": {\n            "value": "hello",\n            "arity": "literal"\n        }\n    },\n    {\n        "value": "=",\n        "arity": "binary",\n        "first": {\n            "value": "b",\n            "arity": "name"\n        },\n        "second": {\n            "value": "function",\n            "arity": "function",\n            "first": [\n                {\n                    "value": "x",\n                    "arity": "name"\n                }\n            ],\n            "second": [\n                {\n                    "value": "=",\n                    "arity": "binary",\n                    "first": {\n                        "value": "c",\n                        "arity": "name"\n                    },\n                    "second": {\n                        "value": 3,\n                        "arity": "literal"\n                    }\n                },\n                {\n                    "value": "return",\n                    "arity": "statement",\n                    "first": {\n                        "value": "+",\n                        "arity": "binary",\n                        "first": {\n                            "value": "x",\n                            "arity": "name"\n                        },\n                        "second": {\n                            "value": "c",\n                            "arity": "name"\n                        }\n                    }\n                }\n            ]\n        }\n    }\n]');
+  });
+  
+  test('Prueba statement', function() {
+  INPUT.value = 'var a = 2;\nif(a ===2){}';
+  main();
+  assert.deepEqual(OUTPUT.innerHTML, '[\n    {\n        "value": "=",\n        "arity": "binary",\n        "first": {\n            "value": "a",\n            "arity": "name"\n        },\n        "second": {\n            "value": 2,\n            "arity": "literal"\n        }\n    },\n    {\n        "value": "if",\n        "arity": "statement",\n        "first": {\n            "value": "===",\n            "arity": "binary",\n            "first": {\n                "value": "a",\n                "arity": "name"\n            },\n            "second": {\n                "value": 2,\n                "arity": "literal"\n            }\n        },\n        "second": null,\n        "third": null\n    }\n]');
+  });
+  
+  test('Prueba syntax error', function() {
+  INPUT.value = 'var a = $';
+  main();
+  assert.deepEqual(OUTPUT.innerHTML, '"Syntax error near \'$\'"');
+  });
+  
+  test('Prueba require();', function() {
+  INPUT.value = 'require("hola");';
+  main();
+  assert.deepEqual(OUTPUT.innerHTML, '{\n    "name": "TypeError",\n    "message": "undefined is not a function"\n}');
+  });
+  
+  test('Prueba var=require();', function() {
+  INPUT.value = 'var a = require("hola");';
+  main();'{\n "value": "=",\n "arity": "binary",\n "first": {\n "value": "a",\n "arity": "name"\n },\n "second": {\n "value": "require",\n "arity": "function",\n "first": {\n "value": "hola",\n "arity": "literal"\n }\n }\n}'
+  assert.deepEqual(OUTPUT.innerHTML, '{\n    "name": "TypeError",\n    "message": "undefined is not a function"\n}');
+  });
+});
