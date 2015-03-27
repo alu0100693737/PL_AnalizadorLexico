e9dbd6fa7d96fcbf2d6447bcb3e40fe0290d558e
Ivan Garcia <alu0100693737@ull.edu.es>
2015-03-27 01:27:00 +0000

    Herouku create

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-{
-"name": "Node.js Getting Started",
-"description": "A barebones Node.js app using Express 4",
-"repository": "https://github.com/heroku/node-js-getting-started",
-"logo": "http://node-js-sample.herokuapp.com/node.svg",
-"keywords": ["node", "express", "static"]
-}
+var _ = require('underscore');
+var $ = require('jquery');
+var express = require('express');
+var app = express();
+var path = require('path');
+var expressLayouts = require('express-ejs-layouts');
+app.set('port', (process.env.PORT || 5000));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+app.use(express.static(__dirname + '/'));
+app.get('/', function (request, response) {
+response.render('index', { title: 'Analizador lexico' });
+});
+app.listen(app.get('port'), function () {
+console.log("Node app is running at localhost:" + app.get('port'));
+});
--- karma.conf.js
+++ karma.conf.js
@@ -1,62 +1,60 @@
 // Karma configuration
-// Generated on Wed Feb 25 2015 19:54:40 GMT+0000 (WET)
+
 module.exports = function(config) {
-config.set({
-plugins: [
-'karma-jquery',
-'karma-mocha',
-'karma-chrome-launcher',
-'karma-firefox-launcher',
-'karma-phantomjs-launcher',
-'karma-html2js-preprocessor',
-'karma-sinon'
-],
+  config.set({
 // base path that will be used to resolve all patterns (eg. files, exclude)
-basePath: '',
+      basePath: '',
 // frameworks to use
 // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-frameworks: ['mocha','jquery-1.8.3','sinon'],
-client: {
-mocha: {
-ui: 'tdd'
-}
-},
+      frameworks: ['mocha', 'chai'],
+      client: {
+	  mocha: {
+	    ui: 'tdd'
+	  }
+      },
+
 // list of files / patterns to load in the browser
-files: [
-'tests/index.html',
-'tests/*.js',
-'*.js',
-'https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.2/underscore-min.js',
-'https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'
-],
-// list of files to exclude
-exclude: [
-'gulpfile.js',
-'index.js'
-],
-// preprocess matching files before serving them to the browser
-// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-preprocessors: {
-'tests/index.html': ['html2js']
-},
-// test results reporter to use
-// possible values: 'dots', 'progress'
-// available reporters: https://npmjs.org/browse/keyword/karma-reporter
-reporters: ['progress'],
-// web server port
-port: 9876,
-// enable / disable colors in the output (reporters and logs)
-colors: true,
-// level of logging
-// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-logLevel: config.LOG_INFO,
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch: true,
-// start these browsers
-// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-browsers: ['Chrome', 'Firefox' , 'PhantomJS'],
-// Continuous Integration mode
-// if true, Karma captures browsers, runs the tests and exits
-singleRun: false
-});
+      files: [
+	'htpp://ajax.googleapis.com/ajax/libs/angularjs/1.0.5/angular.min.js',
+	'https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js',
+	'https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.2/underscore-min.js',
+	'tests/index.html',
+	'*.js',
+	'tests/*.js'
+      ],
+      // list of files to exclude
+      exclude: [
+	'gulpfile.js',
+	
+      ],
+      // preprocess matching files before serving them to the browser
+      // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+      preprocessors: {
+	'tests/index.html': ['html2js']
+      },
+      // test results reporter to use
+      // possible values: 'dots', 'progress'
+      // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+      reporters: ['progress'],
+      // web server port
+      port: 9876,
+      // enable / disable colors in the output (reporters and logs)
+      colors: true,
+      // level of logging
+      // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+      logLevel: config.LOG_DEBUG,
+      // enable / disable watching file and executing tests whenever any file changes
+      autoWatch: true,
+      // start these browsers
+      // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+      browsers: ['PhanthomJS'],
+      
+      // If browser does not capture in given timeout [ms], kill it
+      captureTimeout: 60000,
+      
+      
+      // Continuous Integration mode
+      // if true, Karma captures browsers, runs the tests and exits
+      singleRun: false
+  });
 };
